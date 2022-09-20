4b0a435bc388f020d4d9462e3740caea932d4399
EderDSouza <velasquessouza@gmail.com>
2022-09-19 23:43:15 -0300

    Falta completar acentos

--- script.js
+++ script.js
@@ -5,24 +5,35 @@ function clicar(strin){
     window.document.getElementById("tela-resposta").innerHTML=guardaStrin + strin;
     
 
-}
+};
 
 function teclaNumero(num){
     var guardaNumero=window.document.getElementById("tela-resposta").innerHTML;
     window.document.getElementById("tela-resposta").innerHTML=guardaNumero + num;
-}
+};
 
 function voltar( ){
     var voltarcaracter = window.document.getElementById("tela-resposta").innerHTML;
     window.document.getElementById("tela-resposta").innerHTML=voltarcaracter.substring(0,voltarcaracter.length-1);
-}
+};
 
 function clicarFixa(){
+    var maiscula = window.document.getElementById("tela-resposta").innerHTML;
+    if(maiscula){
+        window.document.getElementById("tela-resposta").innerHTML=maiscula +maiscula.toLocaleUpperCase() ;
+    };
     
+};
 
-}
 function clean(){
-    var limparparacteres= window.document.getElementById("tela-resposta").innerHTML= "";
+    window.document.getElementById("tela-resposta").innerHTML= "";
     
 
-}
+};
+
+function teclaSoma(){
+    var somar= window.document.getElementById("tela-resposta").innerHTML;
+    if(somar){
+        document.getElementById("tela-resposta").innerHTML= eval(somar);
+    };
+};
